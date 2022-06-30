ee8c5c8c4d1b98df1c47f47dcc26f17dcc13cc46
ury <4493070+urysegal@users.noreply.github.com>
2022-06-29 19:46:57 -0700

    WIP

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+import argparse
 import MOvisualizer.plot_file_generator
 import MOvisualizer.Basis_Set_Loader
 
@@ -7,7 +8,15 @@ import MOvisualizer.Basis_Set_Loader
 def main():
     logging.basicConfig(level=logging.INFO)
     logging.info(f"Command line arguments: {sys.argv}")
-    plot = MOvisualizer.plot_file_generator.Plot_File_Generator(2.0, 50, "/tmp/plot.txt")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("radius", help="radius of volume around origin to display",
+                        type=float)
+    parser.add_argument("grid", help="number of axis subdivisions",
+                        type=int)
+    parser.add_argument("output_file", help="output file name",
+                        type=str)
+    args = parser.parse_args()
+    plot = MOvisualizer.plot_file_generator.Plot_File_Generator(args.radius, args.grid, args.output_file)
     basis_set = MOvisualizer.Basis_Set_Loader.Basis_Set_Loader().load()
     plot.generate(basis_set, [1])
 
